1af8eb3e6ebf366b8f0f4f3883886abda3bef227
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-05-21 13:07:16 +0300

    mv Prices styles to Markets.scss

--- src/components/Markets/Prices.js
+++ src/components/Markets/Prices.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './Prices.css';
 
 const Prices = ({ prices, currency }) =>
   Object.keys(prices).map(coin => (
-    <article className="price" key={coin}>
-      <header className="price__header">{coin}</header>
-      <span className="price__span">{prices[coin][currency]}</span>
+    <article className="markets__price" key={coin}>
+      <header className="header">{coin}</header>
+      <span className="span">{prices[coin][currency]}</span>
     </article>
   ));
 
